4f8be35b05fa75e5b8f7a0afbb9ad3c50d52bf21
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2022-05-14 13:45:19 -0300

    Configurando login na aplicação

---
+++ styles/Global.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+export const Form = styled.form`
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  input { 
+    margin: 5px;
+    height: 30px
+  }
+
+  button { 
+    width:100%;
+    max-width: 185px;
+    height: 30px;
+    background: #dd2;
+    border: none
+  }
+
+`
